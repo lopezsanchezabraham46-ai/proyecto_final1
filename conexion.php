33ad6991f4dde7fcf37a8ad39d33ac7399f0ca55
lopezsanchezabraham46-ai <lopez.sanchez.abraham.46@cbtis118.edu.mx>
2025-12-05 07:42:22 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";     // CAMBIAR si tu usuario es otro
+$pass = "";         // tu contraseña
+$db   = "firmas_db";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
 
